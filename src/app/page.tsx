aaeed740e226b56e747b02b9ec9492e19f578e99
BennyTaccardi <benny.taccardi@gmail.com>
2024-10-02 00:25:24 +0200

    Fixing cookies problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import { Input } from "@/components/ui/input";
-import { getAllItems } from "./actions/items/actions";
 
 export default function Home() {
-  (async () => {
-    const t = await getAllItems();
-    console.log(t);
-  })();
-
   return (
     <div className="fixed flex ">
       <Input type="url" placeholder="url" />
