83768f005faf6efdc3a0df12b3a778fcba8f6e63
BennyTaccardi <benny.taccardi@gmail.com>
2024-10-16 23:59:18 +0200

    fix

--- src/lib/appContext.ts
+++ src/lib/appContext.ts
@@ -1,13 +1,14 @@
 import { collectDefaultMetrics, Counter, Registry } from "prom-client";
 
 const registry = new Registry();
+collectDefaultMetrics({ register: registry });
+
 const requestCounter = new Counter({
   name: "generated_url",
   help: "Total number of generated URLs",
   registers: [registry],
   labelNames: ["short_key"],
 });
-collectDefaultMetrics({ register: registry });
 
 export const appContext = {
   service: {
