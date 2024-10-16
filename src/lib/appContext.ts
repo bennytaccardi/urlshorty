c598eb32e937eb2ba7c892c624b21fdd025c157b
BennyTaccardi <benny.taccardi@gmail.com>
2024-10-17 00:17:03 +0200

    singleton on registry

--- src/lib/appContext.ts
+++ src/lib/appContext.ts
@@ -1,16 +1,33 @@
 import { collectDefaultMetrics, Counter, Registry } from "prom-client";
 
-const registry = new Registry();
-collectDefaultMetrics({ register: registry });
-
-const requestCounter = new Counter({
-  name: "generated_url",
-  help: "Total number of generated URLs",
-  registers: [registry],
-});
-export const appContext = {
+interface AppContext {
   service: {
-    requestCounter,
-  },
-  registry,
-};
+    requestCounter: Counter;
+  };
+  registry: Registry;
+}
+let appContext: AppContext | null = null;
+
+if (!appContext) {
+  const registry = new Registry();
+
+  // Collect default metrics
+  collectDefaultMetrics({ register: registry });
+
+  // Define your custom counter
+  const requestCounter = new Counter({
+    name: "generated_url",
+    help: "Total number of generated URLs",
+    registers: [registry],
+    labelNames: ["short_key"],
+  });
+
+  appContext = {
+    service: {
+      requestCounter,
+    },
+    registry,
+  };
+}
+
+export { appContext };
--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -2,9 +2,9 @@ import { appContext } from "@/lib/appContext";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function handler(req: any, res: any) {
-  res.setHeader("Content-Type", appContext.registry.contentType);
+  res.setHeader("Content-Type", appContext!.registry.contentType);
   try {
-    const metrics = await appContext.registry.metrics();
+    const metrics = await appContext!.registry.metrics();
     res.status(200).send(metrics);
   } catch (err) {
     res.status(500).send(`Error collecting metrics: ${(err as Error).message}`);
