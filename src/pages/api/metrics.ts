8b2d503cb96d53d4e82096ed6c6cc945ede3abe2
BennyTaccardi <benny.taccardi@gmail.com>
2024-10-16 23:55:48 +0200

    enhancing api

--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -3,5 +3,10 @@ import { appContext } from "@/lib/appContext";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function handler(req: any, res: any) {
   res.setHeader("Content-Type", appContext.registry.contentType);
-  res.send(await appContext.registry.metrics());
+  try {
+    const metrics = await appContext.registry.metrics();
+    res.status(200).send(metrics);
+  } catch (err) {
+    res.status(500).send(`Error collecting metrics: ${(err as Error).message}`);
+  }
 }
