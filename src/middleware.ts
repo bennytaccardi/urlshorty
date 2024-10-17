ddd65647278b639a5aee4b8c095862b619e5a2c8
BennyTaccardi <benny.taccardi@gmail.com>
2024-10-18 00:01:37 +0200

    come back

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   // Log the current request pathname
-//   const headers = new Headers(request.headers);
-//   headers.set("x-current-hostname", request.nextUrl.hostname);
-//   headers.set("x-current-port", request.nextUrl.port);
-//   headers.set("x-current-protocol", request.nextUrl.protocol);
-//   return NextResponse.next({ request: { headers } });
-// }
+export function middleware(request: NextRequest) {
+  // Log the current request pathname
+  const headers = new Headers(request.headers);
+  headers.set("x-current-hostname", request.nextUrl.hostname);
+  headers.set("x-current-port", request.nextUrl.port);
+  headers.set("x-current-protocol", request.nextUrl.protocol);
+  return NextResponse.next({ request: { headers } });
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
