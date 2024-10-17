6e994da1640404544401d1b891ab02ed5bc411a7
BennyTaccardi <benny.taccardi@gmail.com>
2024-10-17 23:12:29 +0200

    trying global approach

--- src/lib/appContext.ts
+++ src/lib/appContext.ts
@@ -1,19 +1,25 @@
 import { collectDefaultMetrics, Counter, Registry } from "prom-client";
 
-const registry = new Registry();
-collectDefaultMetrics({ register: registry });
+const initAppContext = () => {
+  const registry = new Registry();
+  collectDefaultMetrics({ register: registry });
 
-// Define a custom Counter metric for counting generated URLs
-const requestCounter = new Counter({
-  name: "generated_url",
-  help: "Total number of generated URLs",
-  registers: [registry], // Registering to your custom registry
-});
-requestCounter.inc(10);
-// Exporting appContext with the requestCounter and registry for external use
-export const appContext = {
-  service: {
-    requestCounter,
-  },
-  registry,
+  const requestCounter = new Counter({
+    name: "generated_url",
+    help: "Total number of generated URLs",
+    registers: [registry], // Registering to your custom registry
+  });
+
+  return {
+    service: {
+      requestCounter,
+    },
+    registry,
+  };
 };
+declare global {
+  // eslint-disable-next-line no-var
+  var appContextGlobal: ReturnType<typeof initAppContext> | undefined;
+}
+
+export const appContext = globalThis.appContextGlobal ?? initAppContext();
